67dd9069ef1f311656c174603bddf38f7d9b4dab
Adam Rush <adam.rush@getstream.io>
2022-05-04 16:41:10 +0100

    Initial project

---
+++ Social Contributor/Social_ContributorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_ContributorApp.swift
+//  Social Contributor
+//
+//  Created by Adam Rush on 04/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Social_ContributorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
