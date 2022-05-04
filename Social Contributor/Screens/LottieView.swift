a13eaea0ba95b15014d1648de0488d83bb588c0e
Yash <63893378+YSBoomOfficial@users.noreply.github.com>
2022-05-04 23:05:55 +0100

    Add Lottie View

--- Social Contributor/Screens/LottieView.swift
+++ Social Contributor/Screens/LottieView.swift
@@ -5,4 +5,45 @@
 //  Created by Yash Shah on 04/05/2022.
 //
 
-import Foundation
+import SwiftUI
+import Lottie
+
+struct LottieView: UIViewRepresentable {
+	let lottieFile: String
+
+	let animationView = AnimationView()
+
+	func makeUIView(context: Context) -> some UIView {
+		let view = UIView(frame: .zero)
+
+		animationView.animation = Animation.named(lottieFile)
+		animationView.contentMode = .scaleAspectFit
+		animationView.loopMode = .playOnce
+		animationView.play()
+
+		view.addSubview(animationView)
+
+		animationView.translatesAutoresizingMaskIntoConstraints = false
+
+		NSLayoutConstraint.activate([
+			animationView.heightAnchor.constraint(equalTo: view.heightAnchor),
+			animationView.widthAnchor.constraint(equalTo: view.widthAnchor),
+		])
+
+		return view
+	}
+
+	func updateUIView(_ uiView: UIViewType, context: Context) {
+	}
+}
+
+struct LottieView_Previews: PreviewProvider {
+	static var previews: some View {
+		Group {
+			LottieView(lottieFile: "no-results-found")
+
+			LottieView(lottieFile: "no-results-found")
+				.darkMode()
+		}
+	}
+}
