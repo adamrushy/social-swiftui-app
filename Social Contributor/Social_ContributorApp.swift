343eb522e78310f36aa4e63e31c184d00ceba0ef
Florian Schweizer <post.an.flo@gmail.com>
2022-05-04 19:23:05 +0200

    Add CoreData, Remove Development Team

--- Social Contributor/Social_ContributorApp.swift
+++ Social Contributor/Social_ContributorApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct Social_ContributorApp: App {
+    let persistenceController = PersistenceController.shared
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
