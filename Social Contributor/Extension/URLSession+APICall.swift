8f3a7eb546f927e4d3ef9a342656ba9adae6d597
stanislavk <542557+StanislavK@users.noreply.github.com>
2022-05-08 22:19:18 +0200

    Add extension to download  and decode JSON

--- Social Contributor/Extension/URLSession+APICall.swift
+++ Social Contributor/Extension/URLSession+APICall.swift
@@ -38,3 +38,26 @@ extension URLSession: DataProvider {
 		}
 	}
 }
+
+
+// More info at www.hackingwithswift.com
+// https://tinyurl.com/3e35jzmr
+extension URLSession {
+    func decode<T: Decodable>(
+        _ type: T.Type = T.self,
+        from url: URL,
+        keyDecodingStrategy: JSONDecoder.KeyDecodingStrategy = .useDefaultKeys,
+        dataDecodingStrategy: JSONDecoder.DataDecodingStrategy = .deferredToData,
+        dateDecodingStrategy: JSONDecoder.DateDecodingStrategy = .deferredToDate
+    ) async throws  -> T {
+        let (data, _) = try await data(from: url)
+
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = keyDecodingStrategy
+        decoder.dataDecodingStrategy = dataDecodingStrategy
+        decoder.dateDecodingStrategy = dateDecodingStrategy
+
+        let decoded = try decoder.decode(T.self, from: data)
+        return decoded
+    }
+}
